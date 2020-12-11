646f83224a98886bac58d638f859e21c02d5ba27
Abraham George Smith <abe@abesmith.co.uk>
2020-12-11 11:18:55 +0100

    adding .DS_Store to ignore

--- painter/README.md
+++ painter/README.md
@@ -4,6 +4,9 @@ fbs run
 # to build installer
 fbs clean
 
+** Note ** fbs only supports Python 3.6 so Python 3.6 must be used for the freeze and build steps.
 fbs freeze
+
+** Note ** See install_fixes.py which may need to be ran to fix issues with scikit-image in the built progam prepared by the freeze command.
 
 fbs installer
---
+++ painter/install_fixes.py
@@ -0,0 +1,58 @@
+"""
+Copyright (C) 2020 Abraham George Smith
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import os
+import shutil
+
+def fix_app():
+    """
+    Unfortunately fbs/pyInstaller is no longer capable of building
+    RootPainter on it's own.
+
+    It may be that I upgraded library versions to ones which are no longer
+    supported i.e scikit-image.
+    
+    We will need to copy accross some dependencies to get it working again.
+    """
+    
+    # If you are using Mac, then you will have the following folder after
+    # running the fbs freeze command
+    # ./target/RootPainter.app
+    is_mac = os.path.isdir('./target/RootPainter.app')
+    
+    # If you try to run RootPainter on the command line like so:
+    # ./target/RootPainter.app/Contents/MacOS/RootPainter 
+    # Then you may receive the following error:
+    # File "skimage/feature/orb_cy.pyx", line 12, in init skimage.feature.orb_cy
+    # ModuleNotFoundError: No module named 'skimage.feature._orb_descriptor_positions'
+    
+    # It seems the built application is missing some crucial files from skimage.
+    # To copy these accross we will assume you have an environment created with venv (virtual env)
+    # in the current working directory call 'env'
+    env_dir = './env'
+    assert os.path.isdir(env_dir), f'Could not find env folder {env_dir}'
+    site_packages_dir = os.path.join(env_dir, 'lib/python3.6/site-packages')
+    build_dir = './target/RootPainter.app/Contents/MacOS/'
+    
+    # Copy missing orb file.
+    skimage_dir = os.path.join(site_packages_dir, 'skimage')
+    orb_path = os.path.join(skimage_dir, 'feature/_orb_descriptor_positions.py')
+    shutil.copyfile(orb_path, os.path.join(build_dir, 'skimage/feature/_orb_descriptor_positions.py'))
+
+
+if __name__ == '__main__':
+    fix_app()
+

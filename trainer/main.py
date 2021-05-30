bba44401f327589c189394ab0ff489e187408489
Abraham George Smith <abe@abesmith.co.uk>
2021-05-30 08:35:02 +0200

    import order in main.py

--- trainer/main.py
+++ trainer/main.py
@@ -16,8 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from trainer import Trainer
 import argparse
+from trainer import Trainer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--syncdir',

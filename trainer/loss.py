074e167c1d4ca23e1c0bc870fda9eab3b50cac76
Abraham George Smith <abe@abesmith.co.uk>
2021-05-30 08:33:10 +0200

    pylint loss

--- trainer/loss.py
+++ trainer/loss.py
@@ -14,6 +14,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+# torch has no sum member
+# pylint: disable=E1101
 
 import torch
 from torch.nn.functional import softmax

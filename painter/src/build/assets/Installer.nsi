4edf43a9cceeeacc3b0d4d93d7f85d48ac52e88c
Abraham George Smith <abe@abesmith.co.uk>
2022-07-05 15:35:07 +0200

    updating version number for the client

--- painter/src/build/assets/Installer.nsi
+++ painter/src/build/assets/Installer.nsi
@@ -4,7 +4,7 @@
 !define MUI_UNICON "..\RootPainter\Icon.ico"
 
 !getdllversion "..\RootPainter\RootPainter.exe" ver
-!define VERSION "0.2.13.0"
+!define VERSION "0.2.14.0"
 
 VIProductVersion "${VERSION}"
 VIAddVersionKey "ProductName" "RootPainter"
--- painter/src/main/python/about.py
+++ painter/src/main/python/about.py
@@ -736,10 +736,10 @@ class AboutWindow(QtWidgets.QWidget):
         paper: 'RootPainter: Deep Learning Segmentation of Biological Images with Corrective Annotation'
         <br />
         <br />
-        Version: 0.2.13
+        Version: 0.2.14
         <br />
         <br />
-        Copyright (C) 2020 Abraham George Smith
+        Copyright (C) Abraham George Smith
         <br />
         <br />
         This program is free software: you can redistribute it and/or modify

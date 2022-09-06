60b33cd67b2e9e878e325363826c9a00d31e486a
Abraham Smith <abe@abesmith.co.uk>
2022-09-06 16:50:17 +0200

    list platform plugins when make software

--- painter/make_software.sh
+++ painter/make_software.sh
@@ -1,4 +1,9 @@
 source ./env/bin/activate
 python run_pyinstaller.py
 echo "output executable available at dist/main/main"
+
+echo 'list platforms'
+ls dist/main/PyQt5/Qt5/plugins/platforms
+
+
 rm main.spec # temporary file generated as part of the build process.

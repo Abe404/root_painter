6aed57fb2cba989228447c4b7c50457793a09da3
Abraham Smith <abe@abesmith.co.uk>
2022-09-06 20:00:22 +0200

    delete superfluous echo

--- painter/make_software.sh
+++ painter/make_software.sh
@@ -1,4 +1,3 @@
 source ./env/bin/activate
 python run_pyinstaller.py
-echo "output executable available at dist/main/main"
 rm RootPainter.spec # temporary file generated as part of the build process.

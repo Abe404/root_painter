de9f1f2b67aab8d46193f7e915c6b83b98e5db5a
Abraham Smith <abe@abesmith.co.uk>
2022-09-06 16:15:21 +0200

    pyinstaller compilation for ubuntu

---
+++ painter/compile.py
@@ -0,0 +1,61 @@
+import PyInstaller.__main__
+
+
+# to run this script, first activate the environment.
+# if you don't have an environment.
+# create an environment with
+# python3 -m venv env
+# activate with
+# source env/bin/activate
+# and install environment requirements with 
+# pip install -r requirements.txt
+
+
+# Q: Should we use many arguments passed to pyinstaller, or should we use a spec file?
+"""
+A:
+From: https://github.com/pyinstaller/pyinstaller/blob/fbf7948be85177dd44b41217e9f039e1d176de6b/doc/spec-files.rst
+
+`For many uses of PyInstaller you do not need to examine or modify the spec
+file. It is usually enough to give all the needed information (such as
+hidden imports) as options to the pyinstaller command and let it run.
+
+There are four cases where it is useful to modify the spec file:
+
+1. When you want to bundle data files with the app.
+2. When you want to include run-time libraries (.dll or .so files) that
+   PyInstaller does not know about from any other source.
+3. When you want to add Python run-time options to the executable.
+4. When you want to create a multiprogram bundle with merged common modules.'
+
+
+So the plan for now is to go with more options passed to PyInstaller, and then
+only move onto using a custom spec file when this is recommended by the above
+advice.
+"""
+
+
+# pyinstaller command line argument documentation is available from:
+# https://pyinstaller.org/en/stable/usage.html
+
+
+
+PyInstaller.__main__.run([
+    # --noconfirm: don't ask user to confirm when deleting existing files in dist folder.
+    '--noconfirm',
+
+    # --clean: Clean PyInstaller cache and remove temporary files before building.
+    '--clean',
+
+    # hidden imports added based on this advice:    
+    # https://github.com/pyqtgraph/pyqtgraph/issues/2179
+    '--hidden-import', 'pyqtgraph.graphicsItems.ViewBox.axisCtrlTemplate_pyqt5',
+    '--hidden-import', 'pyqtgraph.graphicsItems.PlotItem.plotConfigTemplate_pyqt5',
+    '--hidden-import', 'pyqtgraph.imageview.ImageViewTemplate_pyqt5',
+
+    # Where to put all the temporary work files .log, .pyz and etc. (default: ./build)
+    '--workpath', 'dist/tmp_files',
+
+    # scriptname: Name of scriptfile to be processed.
+    'src/main/python/main.py'
+])
---
+++ painter/compile.sh
@@ -0,0 +1,32 @@
+source ./env/bin/activate
+python compile.py
+echo "output executable available at dist/main/main"
+
+rm main.spec # temporary file generated as part of the build process.
+
+# from man dpkg-deb
+# dpkl-deb --build creates a debian archive from the filesystem tree stroed in binary-directory.
+# binary-directory must have a DEBIAN subdirectory, which contains the control
+# information files such as the control file itself. These files will be put in the
+# binary package's control information area. 
+
+# So what should a control file contain?
+# From https://www.debian.org/doc/debian-policy/ch-controlfields#binary-package-control-files-debian-control
+# The debian/control file contains the most vital (and version-independent)
+# information about the source package and about the binary packages it creates.
+# The first paragraph of the control file contains information about the source
+# package in general. The subsequent paragraphs each describe a binary package
+# that the source tree builds. Each binary package built from this source package
+# has a corresponding paragraph, except for any automatically-generated debug
+# packages that do not require one.
+
+# Mandatory fields are Package, Version, Architecture, Maintainer and Descript.
+# binary pack control file is located in src/build/DEBIAN_control
+
+mkdir dist/main/DEBIAN
+cp src/build/DEBIAN_control dist/main/DEBIAN/control
+
+dpkg-deb --build dist/main
+mv dist/main.deb dist/RootPainter.deb
+echo 'output debian package to dist/RootPainter.deb'
+

748cc209041de0d04effdf71247d6dff139c4e80
Abraham Smith <abe@abesmith.co.uk>
2023-06-13 14:23:13 +0200

    adding comment for test

--- painter/src/main/python/tests.py
+++ painter/src/main/python/tests.py
@@ -1,9 +1,9 @@
 from PyQt6 import QtCore
 
 def test_mask_function(qtbot):
+    """ test we can click the specify_seg_btn without an error """
     from mask_images import MaskImWidget
     # initialise the mask im widget
     mask_im_widget = MaskImWidget()
     mask_im_widget.show()
     qtbot.mouseClick(mask_im_widget.specify_seg_btn, QtCore.Qt.MouseButton.LeftButton)
-

a682dcfe3a338fe397663c5a99aa9033d5bf06a4
Abraham Smith <abe@abesmith.co.uk>
2023-06-20 10:41:03 +0200

    adding multi epoch data loader with LICENSE

---
+++ trainer/multi_epoch/multi_epoch_loader.py
@@ -0,0 +1,40 @@
+"""
+Copied from: https://github.com/huggingface/pytorch-image-models/blob/d72ac0db259275233877be8c1d4872163954dfbb/timm/data/loader.py#L209-L238
+See LICENSE file in the same folder as this file.
+"""
+
+class MultiEpochsDataLoader(torch.utils.data.DataLoader):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._DataLoader__initialized = False
+        self.batch_sampler = _RepeatSampler(self.batch_sampler)
+        self._DataLoader__initialized = True
+        self.iterator = super().__iter__()
+
+    def __len__(self):
+        return len(self.batch_sampler.sampler)
+
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield next(self.iterator)
+
+
+class _RepeatSampler(object):
+    """ Sampler that repeats forever.
+
+    Args:
+        sampler (Sampler)
+    """
+
+
+    def __init__(self, sampler):
+        self.sampler = sampler
+
+
+    def __iter__(self):
+        while True:
+            yield from iter(self.sampler)
+
+

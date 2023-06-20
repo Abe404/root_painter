d366a83d671c2794e5b35666fbe19ed964f7c32c
Abraham Smith <abe@abesmith.co.uk>
2023-06-20 10:45:43 +0200

    update copyright

--- trainer/loss.py
+++ trainer/loss.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2019 Abraham George Smith
+Copyright (C) 2019-2023 Abraham George Smith
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by

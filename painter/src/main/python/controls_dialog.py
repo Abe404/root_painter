e5d2efc6154f00b948f2510b6d071d4083a638cf
Felipe Galindo <44674864+Felipegalind0@users.noreply.github.com>
2024-05-28 02:54:05 -0500

    Update controls_dialog.py with copyright notice

--- painter/src/main/python/controls_dialog.py
+++ painter/src/main/python/controls_dialog.py
@@ -1,3 +1,20 @@
+"""
+Copyright (C) 2022 Abraham George Smith
+Copyright (C) 2023 Rohan Howard Orton
+Copyright (C) 202$ Felipe Galindo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import sys
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore

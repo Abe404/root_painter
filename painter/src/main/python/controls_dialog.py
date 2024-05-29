79eaecb68fbb35165c3a3c8cb92e710d42acf5b3
Felipe Galindo <44674864+Felipegalind0@users.noreply.github.com>
2024-05-29 16:29:40 -0500

    add english/spanish support to ControlsDialog

--- painter/src/main/python/controls_dialog.py
+++ painter/src/main/python/controls_dialog.py
@@ -26,17 +26,10 @@ class ControlsDialog(QtWidgets.QDialog):
         self.setWindowTitle("Controls")
         self.setMinimumSize(300, 200)
 
-        # controls_text = """
-        # Controls:
-        # - Left Click: Draw
-        # - Right Click: Erase
-        # - Scroll: Zoom
-        # - Arrow Keys: Move around
-        # - Ctrl + Z: Undo
-        # - Ctrl + Y: Redo
-        # """
+        self.settings = QtCore.QSettings("rp", "painter")
+        self.language = self.settings.value("language", "English")
 
-        controls_text = """
+        self.controls_text_en = """
         Controls:
         - Show/Hide Predicted Segmentation: S
         - Show/Hide Human Annotations: A
@@ -56,13 +49,52 @@ class ControlsDialog(QtWidgets.QDialog):
         - Zoom: Scroll
 
         - Pan: cmd + drag the on the image with right click
-        
         """
 
-        layout = QtWidgets.QVBoxLayout()
-        label = QtWidgets.QLabel(controls_text)
-        layout.addWidget(label)
-        self.setLayout(layout)
+        self.controls_text_es = """
+        Controles:
+        - Mostrar/Ocultar Segmentación AI: S
+        - Mostrar/Ocultar Anotacion Humana: A
+        - Mostrar/Ocultar Imagen: I
+
+        - Rojo(Si): Q
+        - Verde(No): W
+        - Borrar: E
+
+        - Tamaño del Pincel: Shift + rueda de el mouse
+
+        - Deshacer: Z        
+        - Rehacer: Shift + cmd + Z
+
+        - Acercar: Shift + '+'
+        - Alejar: -
+        - Zoom: rueda de el mouse
+
+        - mover imagen: cmd + arrastrar sobre la imagen con clic derecho
+        """
+
+        self.layout = QtWidgets.QVBoxLayout()
+
+        self.language_selector = QtWidgets.QComboBox()
+        self.language_selector.addItem("English")
+        self.language_selector.addItem("Spanish")
+        self.language_selector.setCurrentText(self.language)
+        self.language_selector.currentTextChanged.connect(self.update_text)
+
+        self.layout.addWidget(self.language_selector)
+
+        self.label = QtWidgets.QLabel()
+        self.layout.addWidget(self.label)
+
+        self.setLayout(self.layout)
+        self.update_text(self.language)
+
+    def update_text(self, language):
+        if language == "Spanish":
+            self.label.setText(self.controls_text_es)
+        else:
+            self.label.setText(self.controls_text_en)
+        self.settings.setValue("language", language)
 
     def closeEvent(self, event):
         self.closed.emit()

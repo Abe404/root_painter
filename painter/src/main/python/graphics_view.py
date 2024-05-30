ebdcc3b6455dd369d0c350ae846d908634c5ada8
Felipe Galindo <44674864+Felipegalind0@users.noreply.github.com>
2024-05-30 15:14:55 -0500

    fix typo

--- painter/src/main/python/graphics_view.py
+++ painter/src/main/python/graphics_view.py
@@ -32,7 +32,7 @@ class CustomGraphicsView(QtWidgets.QGraphicsView):
     zoom_change = QtCore.pyqtSignal()
 
     def __init__(self, parent=None):
-        super().__init__(parent)
+        super().__init__()
         self.zoom = 1
         self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
         self.grabGesture(Qt.PinchGesture)

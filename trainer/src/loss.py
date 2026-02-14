9f7c2e42c82528c0ac6d0df9a0ecc8752fa2ab16
Abraham Smith <abe@abesmith.co.uk>
2026-02-14 16:57:04 +0100

    fix combined_loss2: use dice_loss2 and add missing binary_cross_entropy import

--- trainer/src/loss.py
+++ trainer/src/loss.py
@@ -18,6 +18,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import torch
 from torch.nn.functional import softmax
 from torch.nn.functional import cross_entropy
+from torch.nn.functional import binary_cross_entropy
 
 
 def dice_loss(predictions, labels):
@@ -45,7 +46,6 @@ def combined_loss(predictions, labels):
     return 0.3 * cross_entropy(predictions, labels)
 
 
-## Ideas for next versions of these loss functions. 
 
 def dice_loss2(preds, labels):
     """ based on loss function from V-Net paper """
@@ -58,16 +58,14 @@ def dice_loss2(preds, labels):
     union = torch.sum(preds) + torch.sum(labels)
     return 1 - ((2 * intersection) / (union))
 
-def combined_loss2(preds, labels, mask=None):
-    """ mix of dice and cross entropy """
-    if mask is not None:
-        preds  = torch.mul(preds, mask) # weighted by defined region of annotation
 
-    # if they are bigger than 1 you get a strange gpu error
-    # without a stack track so you will have no idea why.
+def combined_loss2(preds, labels, mask=None):
+    """ mix of dice and BCE for single-channel sigmoid output.
+        Not yet used - needs benchmarking against combined_loss before switching. """
+    if mask is not None:
+        preds = torch.mul(preds, mask)
     assert torch.max(labels) <= 1
-    cx = (0.3 * binary_cross_entropy(preds, labels))
+    cx = 0.3 * binary_cross_entropy(preds, labels)
     if torch.sum(labels) > 0:
-        dl = dice_loss(preds, labels)
-        return dl + cx
+        return dice_loss2(preds, labels) + cx
     return cx
